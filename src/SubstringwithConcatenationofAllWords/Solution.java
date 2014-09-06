5eca5db612cc10c25abd91940ee037a60e8a222b
fangzhou <fangzhou.ark37@gmail.com>
2014-09-05 21:54:39 -0400

    Substring with Concatenation of All Words

---
+++ src/SubstringwithConcatenationofAllWords/Solution.java
@@ -0,0 +1,49 @@
+package SubstringwithConcatenationofAllWords;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Author: fangzhou
+ * Date:   9/5/14.
+ */
+public class Solution {
+    public List<Integer> findSubstring(String S, String[] L) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(L.length == 0) {
+            return result;
+        }
+        HashMap<String, Integer> m = new HashMap<String, Integer>();
+        for(String l : L) {
+            if(m.containsKey(l)) {
+                m.put(l, m.get(l)+1);
+            } else {
+                m.put(l, 1);
+            }
+        }
+        int wlen = L[0].length();
+        HashMap<String, Integer> tmp = new HashMap<String, Integer>();
+        for(int i = 0; i <= S.length()-wlen*L.length; i++) {
+            tmp.clear();
+            for(int j = i; j <= i+(L.length-1)*wlen; j+=wlen) {
+                String w = S.substring(j, j+wlen);
+                if(!m.containsKey(w)) {
+                    break;
+                }
+                if(tmp.containsKey(w)) {
+                    tmp.put(w, tmp.get(w)+1);
+                } else {
+                    tmp.put(w, 1);
+                }
+                if(tmp.get(w) > m.get(w)) {
+                    break;
+                }
+                if(j == i+(L.length-1)*wlen) {
+                    result.add(i);
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/SubstringwithConcatenationofAllWords/SubstringwithConcatenationofAllWordsTest.java
@@ -0,0 +1,24 @@
+package SubstringwithConcatenationofAllWords;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Author: fangzhou
+ * Date:   9/5/14.
+ */
+public class SubstringwithConcatenationofAllWordsTest {
+    @Test
+    public void testFindSubstring() throws Exception {
+        Solution s = new Solution();
+        String ss = "barfoothefoobarman";
+        String[] l = new String[] {"foo", "bar"};
+        List<Integer> ll = s.findSubstring(ss, l);
+        Assert.assertEquals(ll.size(), 2);
+        Assert.assertEquals((Integer)ll.get(0), Integer.valueOf(0));
+        Assert.assertEquals((Integer)ll.get(1), Integer.valueOf(9));
+
+    }
+}
